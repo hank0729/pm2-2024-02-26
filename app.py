7e2486bbce2e7e826b7f0b82797f42669126021b
hank0729 <132150153+hank0729@users.noreply.github.com>
2024-02-26 15:23:36 +0800

    所有資料

---
+++ app.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+DATABASE = 'data.db'
+
+def get_db_connection():
+    con = sqlite3.connect(DATABASE)
+    con.row_factory = sqlite3.Row
+    return con
+
+def init_db():
+    with get_db_connection() as con:
+        cursor = con.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS data (
+                id INTEGER PRIMARY KEY,
+                time STRING,
+                sensor STRING,
+                pm STRING
+            )
+        ''')
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS fan (
+                id INTEGER PRIMARY KEY,
+                fan STRING
+            )
+        ''')
+
+init_db()
+
+@app.route('/')
+def sign():
+    return render_template('index.html')
+
+@app.route('/data/get/<string:sensor>/<string:pm>')
+def data(sensor, pm):
+    con = get_db_connection()
+    cursor = con.cursor()
+    cursor.execute("INSERT INTO data (time, sensor, pm) VALUES (?, ?, ?)", 
+                   (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), sensor, pm))
+    con.commit()
+    con.close()
+    return "OK"
+
+
+@app.route('/fan/<string:fan>')
+def fans(fan):
+    con = get_db_connection()
+    cursor = con.cursor()
+    cursor.execute("INSERT INTO fan (fan) VALUES (?)", (fan))
+    con.commit()
+    con.close()
+    return "OK"
+
+@app.route('/data/fetch')
+def fetch_data():
+    con = get_db_connection()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM data ORDER BY time DESC")
+    data_records = cursor.fetchall()
+    con.close()
+    return jsonify([dict(ix) for ix in data_records])
+
+@app.route('/data/fetch/fan/latest')
+def fetch_latest_fan_data():
+    con = get_db_connection()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM fan ORDER BY id DESC LIMIT 1")
+    data_record = cursor.fetchone()
+    con.close()
+    return jsonify(dict(data_record))
+
+@app.route('/data/fetch/pm/latest')
+def fetch_latest_pm_data():
+    con = get_db_connection()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM data ORDER BY id DESC LIMIT 1")
+    data_record = cursor.fetchone()
+    con.close()
+    return jsonify(dict(data_record))
+
+
+@app.route('/data/del')
+def del_data():
+    con = get_db_connection()
+    cursor = con.cursor()
+    cursor.execute("DELETE FROM data")  
+    cursor.execute("DELETE FROM fan") 
+    cursor.execute("INSERT INTO fan (fan) VALUES (?)", ('0'))
+    cursor.execute("INSERT INTO data (time, sensor, pm) VALUES (?, ?, ?)", (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '0', '0'))
+    con.commit() 
+    con.close()
+    return "Data deleted successfully"  
+
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
---
+++ static/main.js
@@ -0,0 +1,124 @@
+var timeElement = document.getElementById("time");
+
+function updateTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var ampm = hours >= 12 ? '下午' : '上午';
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    var formattedTime = ampm + ' ' + hours + ':' +
+        (minutes < 10 ? "0" : "") + minutes + ':' +
+        (seconds < 10 ? "0" : "") + seconds;
+    timeElement.textContent = formattedTime;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+function openModal(area) {
+    document.getElementById('myModal').style.display = 'block';
+    fetch('/data/fetch')
+        .then(response => response.json())
+        .then(data => {
+            data.sort((a, b) => new Date(a.time) - new Date(b.time));
+        
+            const labels = data.map(item => item.time);
+            const pmValues = data.map(item => item.pm);
+            const sensorValues = data.map(item => item.sensor);
+        
+            const ctx = document.getElementById('dataChart').getContext('2d');
+            if (window.myChart instanceof Chart) {
+                window.myChart.destroy(); 
+            }
+            window.myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: 'PM Value',
+                            backgroundColor: 'rgb(255, 99, 132)',
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: pmValues,
+                            fill: false, 
+                        },
+
+                        {
+                            label: 'Sensor Value',
+                            backgroundColor: 'rgb(132, 99, 255)',
+                            borderColor: 'rgb(132, 99, 255)',
+                            data: sensorValues,
+                            fill: false,
+                        },
+                    ]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        });
+        
+}
+
+
+
+window.onclick = function(event) {
+    var modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = "none"; 
+    }
+}
+
+function updateFanStatus() {
+    fetch('/data/fetch/fan/latest')
+    .then(response => response.json())
+    .then(data => {
+        const fanStatus = data.fan; 
+        const fanElement = document.getElementById('fan2');
+        if (fanStatus === 1) {
+            fanElement.style.backgroundColor = "rgba(0, 255, 0, 0.795)";
+        } else if (fanStatus === 0) {
+            fanElement.style.backgroundColor = "rgba(255, 0, 0, 0.795)";
+        }
+    })
+    .catch(error => console.error('Error fetching latest fan status:', error));
+}
+document.addEventListener('DOMContentLoaded', function() {
+    updateFanStatus();
+
+});
+
+
+setInterval(updateFanStatus, 1000);
+
+function updatePM2_5Status() {
+    fetch('/data/fetch/pm/latest')
+    .then(response => response.json())
+    .then(data => {
+        const pm2_5Value = data.pm;
+        const pm2Element = document.getElementById('pm2');
+        
+        if (pm2_5Value < 1050) {
+            pm2Element.style.backgroundColor = "rgba(0, 255, 0, 0.795)";
+        } else if (pm2_5Value >= 1050 && pm2_5Value < 3000) {
+            pm2Element.style.backgroundColor = "rgb(255, 238, 0)";
+        } else if (pm2_5Value >= 3000) {
+            pm2Element.style.backgroundColor = "rgba(255, 0, 0, 0.795)";
+        }
+    })
+    .catch(error => console.error('Error fetching latest PM2.5 status:', error));
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    updatePM2_5Status();
+});
+
+setInterval(updatePM2_5Status, 1000);
